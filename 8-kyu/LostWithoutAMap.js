3e945b12098f1faada5afcde0214bfa78e27d564
Maksymenko Danylo <danilmaksimenko000@gmail.com>
2025-05-02 14:03:53 +0200

    LostWithoutAMap ex.

---
+++ 8-kyu/LostWithoutAMap.js
@@ -0,0 +1,16 @@
+// Given an array of integers, return a new array with each value doubled.
+
+// For example:
+
+// [1, 2, 3] --> [2, 4, 6]
+
+function maps(x) {
+  // return x.map(el => el * 2);
+  const res = [];
+
+  for (const num of x) {
+    res.push(num * 2);
+  }
+
+  return res;
+}
